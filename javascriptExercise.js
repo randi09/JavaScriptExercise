28dbffc051a26be6f4b288bfc082d41440f41139
Randi Reyna <77865204+randi09@users.noreply.github.com>
2021-03-29 14:58:02 -0500

    another one

---
+++ javascriptExercise.js
@@ -0,0 +1,49 @@
+// Define Variables
+let newVariable = "Hello JavaScript"
+// define an array in javascript
+let newArray = []
+// define an object in javascript
+
+// define an integer
+let integer = 9
+// define a string
+let otherVariable = "This is a new string"
+
+let anotherVariable = (integer * 5)
+
+let otherArray = ["swag", 123123, true, false, "boolean"]
+console.log(otherArray[1])
+console.log(otherArray[3])
+console.log(otherArray[4])
+
+
+
+newObject = {name:"Randi",
+    age:32,
+    height:5.8, 
+    metric:"ft",
+}
+newObject1 = {name: "Mari",
+    age:38,
+    height:5.3, 
+    metric:"ft",
+    sport: "Softball",
+}
+newObject2 = {name: "Mark",
+    age:32,
+    height:1.2, 
+    metric:"meters",
+}
+newObject3 = {name: "Amanda",
+    age:50,
+    height:5.7, 
+    metric:"ft",
+}
+newObject4 = {name: "James", 
+    age:50,
+    height:6.3, 
+    metric:"ft",
+   }
+console.log(newObject.name)
+console.log(newObject4.age)
+console.log(newObject2.height)
